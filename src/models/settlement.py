b05b7f3e4b6e35ad9b584b7056f1fb6e3b45feae
josojo <josojo@hotmail.de>
2022-05-12 11:25:37 +0100

    all files

---
+++ src/models/settlement.py
@@ -0,0 +1,72 @@
+
+import json
+from src.models.order import Order, OrdersSerializedType
+from src.models.token import (
+    Token,
+)
+from typing import Any, Optional
+
+class InteractionData:
+
+    def __init__(
+        self,
+        target: str,
+        call_data: str,
+    ):
+        self.value = '0'
+        self.target = target
+        self.call_data = call_data
+
+    def as_dict(self) :
+        """Return Order object as dictionary."""
+        return  {
+                'target': str(self.target),
+                'value': '0',
+                'call_data': self.call_data,
+                }
+        
+        
+
+    def __dict__(self) -> str:
+        """Represent as string."""
+        return json.dumps(self.as_dict(), indent=2)
+
+
+class Settlement:
+    """Class to represent a batch auction."""
+
+    def __init__(
+        self,
+        ref_token: Token,
+        name: str = "batch_auction",
+        metadata: Optional[dict] = None,
+    ):
+        self.name = name
+        self.metadata = metadata if metadata else {}
+        self.interaction_data = []
+        self._orders = []
+        self.ref_token = ref_token
+        self.prices = { ref_token: "1000000000000000000"}
+    
+    def as_dict(self) :
+        """Return Order object as dictionary."""
+        # Currently, only limit buy or sell orders be handled.
+        return {
+            "name": str(self.name),
+            "metadata": str(self.metadata),
+            "orders": {order.order_id: order.as_dict() for order in self.orders},
+            "interaction_data": [ interaction.as_dict() for interaction in self.interaction_data],
+            "prices": self.prices,
+        }
+
+    def __dict__(self) -> str:
+        """Represent as string."""
+        return json.dumps(self.as_dict(), indent=2)
+
+    def add_payload(self, target, call_data):
+        data = InteractionData(target, call_data)
+        self.interaction_data.append(data)
+
+    def add_order(self, order):
+        self.orders.append(order)
+    
---
+++ src/oneinch/oneinch.py
@@ -0,0 +1,124 @@
+import json
+import requests
+from decimal import Decimal
+
+class OneInchExchange:
+
+    base_url = 'https://api.1inch.io'
+
+    chains = dict(
+        ethereum = '1',
+        gchain = '100',
+    )
+
+    versions = dict(
+        # v2 = "v2.0",
+        v2_1 = "v4.0"
+    )
+
+    endpoints = dict(
+        swap = "swap",
+        quote = "quote",
+        tokens = "tokens",
+        protocols = "protocols",
+        protocols_images = "protocols/images",
+        approve_spender = "approve/spender",
+        approve_calldata = "approve/calldata"
+    )
+
+    tokens = dict()
+    tokens_by_address = dict()
+    protocols = []
+    protocols_images = []
+
+    def __init__(self, address, chain='gchain'):
+        self.address = address
+        self.version = 'v4.0'
+        self.chain_id = self.chains[chain]
+        self.chain = chain
+        # self.get_tokens()
+        # self.get_protocols()
+        # self.get_protocols_images()
+
+
+    def _get(self, url, params=None, headers=None):
+        """ Implements a get request """
+        try:
+            response = requests.get(url, params=params, headers=headers)
+            payload = json.loads(response.text)
+            data = payload
+        except requests.exceptions.ConnectionError as e:
+            print("ConnectionError when doing a GET request from {}".format(url))
+            data = None
+        return data
+
+
+    def health_check(self):
+        url = '{}/56/{}/healthcheck'.format(
+            self.base_url, self.chain_id)
+        response = requests.get(url)
+        result = json.loads(response.text)
+        if not result.__contains__('status'):
+            return result
+        return result['status']
+
+
+    def get_tokens(self):
+        url = '{}/{}/{}/tokens'.format(
+            self.base_url, self.version, self.chain_id)
+        result = self._get(url)
+        if not result.__contains__('tokens'):
+            return result
+        for key in result['tokens']:
+            token = result['tokens'][key]
+            self.tokens_by_address[key] = token
+            self.tokens[token['symbol']] = token
+        return self.tokens
+
+
+    def get_protocols(self):
+        url = '{}/{}/{}/protocols'.format(
+            self.base_url, self.version, self.chain_id)
+        result = self._get(url)
+        if not result.__contains__('protocols'):
+            return result
+        self.protocols = result
+        return self.protocols
+
+
+    def get_protocols_images(self):
+        url = '{}/{}/{}/protocols/images'.format(
+            self.base_url, self.version, self.chain_id)
+        result = self._get(url)
+        if not result.__contains__('protocols'):
+            return result
+        self.protocols_images = result
+        return self.protocols_images
+
+
+    def get_quote(self, from_address:str, to_address:str, amount:int):
+        url = '{}/{}/{}/quote'.format(
+            self.base_url, self.version, self.chain_id)
+        url = url + '?fromTokenAddress={}&toTokenAddress={}&amount={}&fromAddress=0x9008D19f58AAbD9eD0D60971565AA8510560ab41&slippage=50'.format(
+            from_address, 
+            to_address, 
+            amount,
+            )
+        result = self._get(url)
+        return result
+
+    def get_swap(self, from_address:str, to_address:str, amount:int):
+        url = '{}/{}/{}/swap'.format(
+            self.base_url, self.version, self.chain_id)
+        url = url + '?fromTokenAddress={}&toTokenAddress={}&amount={}&fromAddress=0x9008D19f58AAbD9eD0D60971565AA8510560ab41&slippage=50'.format(
+            from_address, 
+            to_address, 
+            amount,
+            )
+        result = self._get(url)
+        return result   
+
+
+    def convert_amount_to_decimal(self, token_symbol, amount):
+        decimal = self.tokens[token_symbol]['decimals']
+        return Decimal(amount) / Decimal(10**decimal)
